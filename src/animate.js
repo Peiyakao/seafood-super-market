8f28559530b0a411edfebb433f0927ad60826ce8
Peiyakao <cubearcu@gmail.com>
2023-03-14 12:00:58 +0800

    add init

---
+++ src/animate.js
@@ -0,0 +1,24 @@
+let nickname ="avv";
+if (!nickname || nickname.length <= 4){
+    console.log("please write sth");
+}else {
+    console.log("welcome");
+};
+let nickname2 ="";
+if (nickname2 === null || nickname2=== "" || nickname2 === undefined){
+    console.log("please write sth");
+}else {
+    console.log("welcome");
+};
+
+let prawhile =6;
+do{
+  console.log("print");
+  prawhile++;  
+}while(prawhile<5);
+// using while to printout 1~10
+let i =0;
+while(i <10){
+    i++;
+    console.log(i);
+};
